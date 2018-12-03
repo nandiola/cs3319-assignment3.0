8931ef096c0da017257d63d6e4d54fe06c987509
Anirudha Nandi <nandiola17@gmail.com>
2018-12-03 17:43:00 -0500

    Update getdata.php

--- phpworkshop/getdata.php
+++ phpworkshop/getdata.php
@@ -1,14 +1,19 @@
 <?php
 $query = "SELECT * FROM pet";
+$query2 = "SELECT * FROM owner";
 $result = mysqli_query($connection,$query);
+$result2 = mysqli_query($connection,$query2);
+
 if (!$result) {
     die("databases query failed.");
 }
+
 echo "<ol>";
 while ($row = mysqli_fetch_assoc($result)) {
+    $owner =  mysqli_fetch_assoc($result2);
     echo "<li>";
-    echo $row["species"] ."--".$row["petname"]. "</li>";
-}
+    echo $row["ownerid"] ."--".$row["petname"].$owner["ownerid"]. "</li>";
+    }
 mysqli_free_result($result);
 echo "</ol>";
 ?>
