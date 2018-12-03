198292b5a6f5edab4cc17f0b5b8430c938672581
Anirudha Nandi <nandiola17@gmail.com>
2018-12-03 17:26:51 -0500

    Update getdata.php

--- phpworkshop/getdata.php
+++ phpworkshop/getdata.php
@@ -4,9 +4,11 @@ $result = mysqli_query($connection,$query);
 if (!$result) {
     die("databases query failed.");
 }
+echo "<ol>";
 while ($row = mysqli_fetch_assoc($result)) {
     echo "<li>";
     echo $row["species"] . "</li>";
 }
 mysqli_free_result($result);
+echo "</ol>";
 ?>
