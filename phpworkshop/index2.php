bd1f23d5b156d974053b3445ae17b802eec4e849
Anirudha Nandi <nandiola17@gmail.com>
2018-12-03 17:12:02 -0500

    PHP Workshop

---
+++ phpworkshop/index2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Dr. Western's Vet Clinic</title>
+</head>
+<body>
+<h1>Welcome to the Western Vet Clinic</h1>
+<h2>Pets we look after</h2>
+<ol>
+<li>Dogs</li>
+<li>Cats</li>
+<li>Birds</li>
+</ol>
+</body>
+</html>
