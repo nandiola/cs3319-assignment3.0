40cc769b92350e6371abb2e57c70d0d0807eb382
Anirudha Nandi <nandiola17@gmail.com>
2018-12-03 18:15:45 -0500

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -14,8 +14,7 @@
 
         <h3>Question 1</h3>
         <form action = "question1.php" method = "post">
-			Select from any of these customers to see which products they purchased <br />
-			<input type="radio" name="customerid" value="31">Sideshow Bob, Springfield, 647-3849, AgentID: 66<br><input type="radio" name="customerid" value="12">Monty Burns, Springfield, 789-0001, AgentID: 99<br><input type="radio" name="customerid" value="15">Fred Flintstone, Boston, 647-3849, AgentID: 11<br><input type="radio" name="customerid" value="14">Peter Griffens, Providence, 987-4567, AgentID: 99<br><input type="radio" name="customerid" value="32">Shah Khann, Oakville, 647-3849, AgentID: 12<br><input type="radio" name="customerid" value="10">Barney Rubble, Boston, 777-1234, AgentID: 11<br><input type="radio" name="customerid" value="21">Homer Simpson, Springfield, 686-8956, AgentID: 99<br><input type="radio" name="customerid" value="13">Wonder Woman, Kypto, 888-1234, AgentID: 33<br>	
+            <?php include 'question1.php'; ?>	
 			<br />
 			<input type = "submit" value= "Show Purchases!"/>
 		</form>
