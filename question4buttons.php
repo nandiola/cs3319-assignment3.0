b6d022a759af1c8c5d9919ec0506895a01124cfa
Anirudha Nandi <nandiola17@gmail.com>
2018-12-03 20:43:08 -0500

    Update question4buttons.php

--- question4buttons.php
+++ question4buttons.php
@@ -2,16 +2,16 @@
 
         echo "<h5>Enter customer</h5>";
 
-        echo '<br><br>Customer first name: <input type="text" name="firstname"><br>';
+        echo 'Customer first name: <input type="text" name="firstname"><br>';
 
-        echo '<br><br>Customer last name name: <input type="text" name="lastname"><br>';
+        echo 'Customer last name name: <input type="text" name="lastname"><br>';
 
-        echo '<br><br>Customer city: <input type="text" name="city_located"><br>';
+        echo 'Customer city: <input type="text" name="city_located"><br>';
 
-        echo '<br><br>Customer Agent ID: <input type="text" name="agent_id"><br>';
+        echo 'Customer Agent ID: <input type="text" name="agent_id"><br>';
 
-        echo '<br><br>Customer phone number: <input type="text" name="phone_number"><br>';
+        echo 'Customer phone number: <input type="text" name="phone_number"><br>';
         
-        echo '<br><br>Customer ID (cannot be the same as an existing ID): <input type="text" name="customer_id"><br>';
+        echo 'Customer ID (cannot be the same as an existing ID): <input type="text" name="customer_id"><br>';
 
 ?>
