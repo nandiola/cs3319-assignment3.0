4fe36f405baf232709ad6ec2a11d41e481a255ab
Anirudha Nandi <nandiola17@gmail.com>
2018-12-03 23:49:55 -0500

    Update question5_buttons.php

--- question5_buttons.php
+++ question5_buttons.php
@@ -13,10 +13,11 @@ if (!$result) {
 
 
         while ($row = mysqli_fetch_assoc($result)) {
-                echo '<input type="radio" name="customer_id" value="';
-                echo $row["customer_id"];
-                echo '"><br>' .$row["firstname"] ." " .$row["lastname"] .", " .$row["city_located"] .", " .$row["phone_number"]; 
-
+                echo '<input type="radio" name="phone_number" value="';
+                echo $row["phone_number"];
+                echo '">' .$row["firstname"] ." " .$row["lastname"] .", " .$row["city_located"]; 
+                echo '<input type="hidden" name="customer_id" value="'.$row["customer_id"];
+                echo '"><br>';
         }
 
 mysqli_free_result($result);
