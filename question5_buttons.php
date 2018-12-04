a5cb586a87ed531965f6a07165809c624652fbba
Anirudha Nandi <nandiola17@gmail.com>
2018-12-03 23:47:16 -0500

    Update question5_buttons.php

--- question5_buttons.php
+++ question5_buttons.php
@@ -16,7 +16,8 @@ if (!$result) {
                 echo '<input type="radio" name="phone_number" value="';
                 echo $row["customer_id"];
                 echo '">' .$row["firstname"] ." " .$row["lastname"] .", " .$row["city_located"] .", " .$row["phone_number"]; 
-               
+                echo '<input type="hidden" name="random" value="12';
+                echo '"><br>';
         }
 
 mysqli_free_result($result);
